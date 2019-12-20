f7462c5044fda0910d06e1cad0eb7cb65df324a0
Yongjin Park <ypp@csail.mit.edu>
2019-12-20 08:45:15 -0500

    start adding random col

---
+++ src/mmutil_rand_col.cc
@@ -0,0 +1,17 @@
+#include "mmutil_random_col.hh"
+
+void print_help(const char* fname) {
+  std::cerr << "Randomly select columns (samples) to reduce computational cost" << std::endl;
+  std::cerr << std::endl;
+  std::cerr << fname << " num_col mtx_file sample_file output" << std::endl;
+  std::cerr << std::endl;
+}
+
+
+
+int main(const int argc, const char* argv[]) {
+
+
+
+  return EXIT_SUCCESS;
+}
---
+++ src/mmutil_rand_col.hh
@@ -0,0 +1,9 @@
+#include "mmutil.hh"
+#include "io.hh"
+
+#ifndef MMUTIL_RANDOM_COL_HH_
+#define MMUTIL_RANDOM_COL_HH_
+
+
+
+#endif
