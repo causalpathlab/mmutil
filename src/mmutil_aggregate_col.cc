1d0ab0248c227e10bc56f516a43a385a6ed4e67e
Yongjin Park <ypp.ubc@gmail.com>
2020-03-09 08:55:22 -0400

    modified aggregate function

--- src/mmutil_aggregate_col.cc
+++ src/mmutil_aggregate_col.cc
@@ -1,35 +1,18 @@
 #include "mmutil_aggregate_col.hh"
 
-void
-print_help(const char *fname)
-{
-    const char *_desc =
-        "Aggregate columns to create summary stat matrices.\n"
-        "\n"
-        "[Arguments]\n"
-        "MTX        : matrix market file (M x N)\n"
-        "PROB       : N x K probability\n"
-        "OUTPUT     : ${OUTPUT}.s1.gz ${OUTPUT}.s2.gz ${OUTPUT}.n.gz \n"
-        "\n";
-    std::cerr << fname << " MTX COL_MTX PROB ROW_PROB OUTPUT" << std::endl;
-    std::cerr << std::endl;
-    std::cerr << _desc << std::endl;
-    std::cerr << std::endl;
-}
-
 int
 main(const int argc, const char *argv[])
 {
-    if (argc != 4) {
-        print_help(argv[0]);
-        return EXIT_FAILURE;
-    }
 
-    const std::string mtx(argv[1]);
-    const std::string prob(argv[2]);
-    const std::string output(argv[3]);
+    aggregate_options_t options;
+    CHECK(parse_aggregate_options(argc, argv, options));
 
-    CHECK(aggregate_col(mtx, prob, output));
+    CHECK(aggregate_col(options.mtx,
+                        options.prob,
+                        options.ind,
+                        options.lab,
+                        options.out,
+                        options.batch_size));
 
     TLOG("Done");
     return EXIT_SUCCESS;
--- src/mmutil_aggregate_col.hh
+++ src/mmutil_aggregate_col.hh
@@ -1,108 +1,241 @@
+#include <getopt.h>
+#include <unordered_map>
 #include "mmutil.hh"
 #include "mmutil_stat.hh"
+#include "eigen_util.hh"
+#include "io.hh"
 
 #ifndef MMUTIL_AGGREGATE_COL_HH_
 #define MMUTIL_AGGREGATE_COL_HH_
 
-struct mat_stat_collector_t {
-    using index_t = Index;
-    using scalar_t = Scalar;
+struct aggregate_options_t {
+    using Str = std::string;
 
-    // Z : clusters x samples
-    // X : dimension x samples
+    typedef enum { UNIFORM, CV, MEAN } sampling_method_t;
+    const std::vector<Str> METHOD_NAMES;
 
-    explicit mat_stat_collector_t(const Mat &zz)
-        : Z(zz)
-        , nn(Z.cols())
-        , kk(Z.rows())
+    aggregate_options_t()
     {
+        mtx = "";
+        prob = "";
+        ind = "";
+        lab = "";
+        out = "output";
+        batch_size = 10000;
+        verbose = false;
     }
 
-    void set_dimension(const index_t D, const index_t n, const index_t e)
-    {
+    Str mtx;
+    Str prob;
+    Str ind;
+    Str lab;
+    Str out;
 
-        if (n != nn) {
-            WLOG("columns(MTX) != rows(Z)");
-        }
-
-        S1.resize(kk, D);
-        S2.resize(kk, D);
-        N.resize(kk, D);
-
-        S1.setZero();
-        S2.setZero();
-        N.setZero();
-
-        // to check empty columns
-        Vec _nnz_col = Z.transpose() * Mat::Ones(Z.rows(), 1);
-        std_vector(_nnz_col, nnz_col);
-
-        TLOG("Start aggregating " << kk << " x " << D << " stats");
-    }
-
-    void eval(const index_t i, const index_t j, const scalar_t x_ij)
-    {
-        if (j < nn && j >= 0 && nnz_col.at(j) > 0) {
-            S1.col(i) += Z.col(j) * x_ij;
-            S2.col(i) += Z.col(j) * x_ij * x_ij;
-            N.col(i) += Z.col(j);
-        }
-    }
-
-    void eval_end() { TLOG("Finished aggregating the stats"); }
-
-    const Mat &Z;
-    const index_t nn;
-    const index_t kk;
-
-    Mat S1;
-    Mat S2;
-    Mat N;
-
-private:
-    std::vector<Index> nnz_col;
+    Index batch_size;
+    bool verbose;
 };
+
+template <typename OPTIONS>
+int
+parse_aggregate_options(const int argc,     //
+                        const char *argv[], //
+                        OPTIONS &options)
+{
+    const char *_usage =
+        "\n"
+        "[Arguments]\n"
+        "--mtx (-m)        : data MTX file (M x N)\n"
+        "--data (-m)       : data MTX file (M x N)\n"
+        "--prob (-p)       : annotation/clustering probability (N x K)\n"
+        "--ind (-i)        : N x 1 sample to individual (n)\n"
+        "--lab (-l)        : K x 1 label name\n"
+        "--out (-o)        : Output file header\n"
+        "\n"
+        "--batch_size (-B) : Batch size (default: 10000)\n"
+        "\n"
+        "[Output]\n"
+        "${output}_${lab}.s1.gz   : (M x n) sum x[i,j] z[j,k]\n"
+        "${output}_${lab}.s2.gz   : (M x n) sum x[i,j]^2 z[j,k]\n"
+        "${output}_${lab}.cols.gz : (n x 1) name of the columns\n"
+        "\n";
+
+    const char *const short_opts = "m:p:i:l:o:B:hv";
+
+    const option long_opts[] =
+        { { "mtx", required_argument, nullptr, 'm' },        //
+          { "data", required_argument, nullptr, 'm' },       //
+          { "prob", required_argument, nullptr, 'p' },       //
+          { "ind", required_argument, nullptr, 'i' },        //
+          { "lab", required_argument, nullptr, 'l' },        //
+          { "label", required_argument, nullptr, 'l' },      //
+          { "out", required_argument, nullptr, 'o' },        //
+          { "batch_size", required_argument, nullptr, 'B' }, //
+          { "batchsize", required_argument, nullptr, 'B' },  //
+          { "verbose", no_argument, nullptr, 'v' },          //
+          { nullptr, no_argument, nullptr, 0 } };
+
+    while (true) {
+        const auto opt = getopt_long(argc,                      //
+                                     const_cast<char **>(argv), //
+                                     short_opts,                //
+                                     long_opts,                 //
+                                     nullptr);
+
+        if (-1 == opt)
+            break;
+
+        switch (opt) {
+        case 'm':
+            options.mtx = std::string(optarg);
+            break;
+        case 'p':
+            options.prob = std::string(optarg);
+            break;
+        case 'i':
+            options.ind = std::string(optarg);
+            break;
+        case 'l':
+            options.lab = std::string(optarg);
+            break;
+        case 'o':
+            options.out = std::string(optarg);
+            break;
+        case 'B':
+            options.batch_size = std::stoi(optarg);
+            break;
+        case 'v': // -v or --verbose
+            options.verbose = true;
+            break;
+        case 'h': // -h or --help
+        case '?': // Unrecognized option
+            std::cerr << _usage << std::endl;
+            return EXIT_FAILURE;
+        default: //
+                 ;
+        }
+    }
+
+    ERR_RET(!file_exists(options.mtx), "No MTX data file");
+    ERR_RET(!file_exists(options.prob), "No PROB data file");
+    ERR_RET(!file_exists(options.ind), "No IND data file");
+    ERR_RET(!file_exists(options.lab), "No LAB data file");
+
+    return EXIT_SUCCESS;
+}
 
 int
 aggregate_col(const std::string mtx_file,
               const std::string prob_file,
-              const std::string output)
+              const std::string ind_file,
+              const std::string lab_file,
+              const std::string output,
+              const Index batch_size = 30000)
 {
 
     Mat Z;
     CHECK(read_data_file(prob_file, Z));
-
     TLOG("Latent membership matrix: " << Z.rows() << " x " << Z.cols());
 
-    Z.transposeInPlace(); // cluster x sample
+    std::vector<std::string> ind;
+    ind.reserve(Z.rows());
+    CHECK(read_vector_file(ind_file, ind));
 
-    //////////////////////////////////////
-    // collect statistics from the data //
-    //////////////////////////////////////
+    ASSERT(ind.size() == Z.rows(),
+           "Individual membership file mismatches with Z");
 
-    TLOG("Might take some time... (but memory-efficient)");
+    std::unordered_map<std::string, Index> group_idx;
+    std::vector<std::string> group_name;
+    std::vector<Index> group;
+    group.reserve(ind.size());
 
-    mat_stat_collector_t collector(Z);
-    visit_matrix_market_file(mtx_file, collector);
-
-    {
-        TLOG("Writing S1 stats");
-        Mat S1 = collector.S1.transpose();
-        write_data_file(output + ".s1.gz", S1);
+    for (Index i = 0; i < ind.size(); ++i) {
+        const std::string &ii = ind.at(i);
+        if (group_idx.count(ii) == 0) {
+            const Index j = group_idx.size();
+            group_idx[ii] = j;
+            group_name.push_back(ii);
+        }
+        group.emplace_back(group_idx.at(ii));
     }
 
-    {
-        TLOG("Writing S2 stats");
-        Mat S2 = collector.S2.transpose();
-        write_data_file(output + ".s2.gz", S2);
-    }
+    const Index Nsample = ind.size();
+    const Index Nind = group_idx.size();
+    const Index K = Z.cols();
 
-    {
-        TLOG("Writing N stats");
-        Mat N = collector.N.transpose();
-        write_data_file(output + ".n.gz", N);
-    }
+    TLOG("Identified " << Nind << " individuals");
 
+    std::vector<std::string> lab;
+    lab.reserve(K);
+    CHECK(read_vector_file(lab_file, lab));
+
+    ASSERT(lab.size() == K,
+           "Need the same number of label names for the columns of Z");
+
+    TLOG("Identified " << K << " labels");
+
+    // Read an expression matrix block by block
+
+    ASSERT(Z.rows() == Nsample, "rows(Z) != Nsample");
+
+    for (Index k = 0; k < K; ++k) {
+
+        TLOG("Aggregating on " << lab.at(k) << "...");
+
+        std::vector<Eigen::Triplet<Scalar>> triples;
+        triples.reserve(Nsample);
+
+        for (Index j = 0; j < Nsample; ++j) {
+            const Scalar pr_jk = Z(j, k);
+            if (pr_jk < 1e-4)
+                continue;
+            triples.emplace_back(Eigen::Triplet<Scalar>(j, group.at(j), pr_jk));
+        }
+
+        SpMat Zk(Nsample, Nind);
+        Zk.setFromTriplets(triples.begin(), triples.end());
+
+        //////////////////////////////////////
+        // collect statistics from the data //
+        //////////////////////////////////////
+
+        Mat S1, S2;
+
+        for (Index lb = 0; lb < Nsample; lb += batch_size) {
+            const Index ub = std::min(Nsample, batch_size + lb);
+            std::vector<Index> subcols_b(ub - lb);
+
+            std::iota(subcols_b.begin(), subcols_b.end(), lb);
+            TLOG("Reading data on the batch [" << lb << ", " << ub << ")");
+
+            SpMat X_b = read_eigen_sparse_subset_col(mtx_file, subcols_b);
+            SpMat Zk_b = row_sub(Zk, subcols_b);
+
+            SpMat S1_b = X_b * Zk_b;                   // feature x Nind
+            SpMat S2_b = X_b.cwiseProduct(X_b) * Zk_b; // feature x Nind
+
+            if (lb == 0) {
+                S1.resize(S1_b.rows(), Nind);
+                S1.setZero();
+                S2.resize(S2_b.rows(), Nind);
+                S2.setZero();
+            }
+
+            S1 += S1_b;
+            S2 += S2_b;
+
+            TLOG("S1 " << S1.rows() << " x " << S1.cols());
+        }
+
+        const std::string clust_name = lab.at(k);
+
+        const std::string out_hdr = output + "_" + clust_name;
+
+        write_vector_file(out_hdr + ".cols.gz", group_name);
+        write_data_file(out_hdr + ".s1.gz", S1);
+        write_data_file(out_hdr + ".s2.gz", S2);
+
+        TLOG("Wrote files for " << lab.at(k));
+    }
     return EXIT_SUCCESS;
 }
 
